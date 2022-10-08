843e65c007908be45ea7bc9f80015453ead084dd
Frolaf <78124286+Frolaf@users.noreply.github.com>
2022-10-07 21:46:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -22,15 +22,35 @@ function setup(){
     canvas = createCanvas(280,280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
 }
 
 function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
     check_sketch();
     if(drawn_sketch == sketch){
         answer_holder = "set";
         score++;
         document.getElementById("score").innerHTML = "score: " + score;
     }
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    draw_sketch = results[0].label;
+    document.getElementById("label").innerHTML = "your sketch: " + drawn_sketch;
+    document.getElementById("confidence").innerHTML = "confidence: " + Math.round(results[0].confidence * 100) + "%";
 }
 
 function check_sketch(){
@@ -46,4 +66,8 @@ function check_sketch(){
         answer_holder = "";
         updateCanvas();
     }
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet')
 }
